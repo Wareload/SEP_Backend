c83214cb6e89a85ae933e513abb08ed27c5bb600
Wardur <haasdl2000@gmail.com>
2022-06-10 01:11:07 +0200

    working on account test

--- src/routes/accountFunctionality.test.ts
+++ src/routes/accountFunctionality.test.ts
@@ -4,22 +4,6 @@ process.env.DB_USER = process.env.DB_USER_TEST
 process.env.DB_PASSWORD = process.env.DB_PASSWORD_TEST
 process.env.DB_DATABASE = process.env.DB_DATABASE_TEST
 
-
-// @ts-ignore
-import mysql from 'mysql-await'
-
-//create mysql connection to export
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
-//need to clear database
-
-jest.resetModules()
 import * as func from "./accountFunctionality"
 
 test("No Test yet",()=>{})
