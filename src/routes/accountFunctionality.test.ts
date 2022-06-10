dcede450ababb3e0ed5f00e88a0a74de3e9a87de
Wareload <developer@wareload.net>
2022-06-10 08:00:50 +0200

    added account tests

--- src/routes/accountFunctionality.test.ts
+++ src/routes/accountFunctionality.test.ts
@@ -8,7 +8,7 @@ process.env.DB_DATABASE = process.env.DB_DATABASE_TEST
 import mysql from 'mysql-await'
 
 //create mysql connection to export
-const connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -22,19 +22,50 @@ import * as func from "./accountFunctionality"
 beforeEach(async () => {
     if (process.env.TEST_DB) {
         try {
-            await connection.awaitQuery("TRUNCATE TABLE invitation")
-            await connection.awaitQuery("TRUNCATE TABLE teammember")
-            await connection.awaitQuery("TRUNCATE TABLE team")
-            await connection.awaitQuery("TRUNCATE TABLE user")
+            await connection.awaitQuery("DELETE FROM invitation")
+            await connection.awaitQuery("DELETE FROM teammember")
+            await connection.awaitQuery("DELETE FROM team")
+            await connection.awaitQuery("DELETE FROM user")
+            await connection.awaitQuery("DELETE FROM sessions")
         } catch (e) {
             console.error(e)
         }
     }
 })
 
+afterAll(() => {
+    connection.end();
+})
 
-test("No Test yet", () => {
+test("Test Is Logged In", () => {
     if (process.env.TEST_DB) {
-
+        expect(func.isLoggedIn(5)).toEqual({status: 200})
+        expect(func.isLoggedIn(9)).toEqual({status: 200})
+        expect(func.isLoggedIn(6)).toEqual({status: 200})
+    }
+})
+test("Test Login", async () => {
+    if (process.env.TEST_DB) {
+        expect(await func.login("1@1.net", "TestTest123")).toEqual({status: 401})
+        expect(await func.login("11.net", "TestTest123")).toEqual({status: 400})
+        expect(await func.login("1@1.net", "TestTestTest")).toEqual({status: 400})
+        await func.register("1@1.net", "TestTestTest123", "Firstname", "Lastname")
+        const result = await func.login("1@1.net", "TestTestTest123");
+        expect(result.status).toBe(200)
+        expect(result.body).toBe(undefined)
+        expect(typeof result.user_id).toBe("number")
+    }
+})
+test("Test Register", async () => {
+    if (process.env.TEST_DB) {
+        const result1 = await func.register("11.net", "TestTestTest123", "Firstname", "Lastname")
+        const result2 = await func.register("1@1.net", "TestTestTest", "Firstname", "Lastname")
+        const result3 = await func.register("1@1.net", "TestTestTest123", "Firstname", "Lastname")
+        const result4 = await func.register("1@1.net", "TestTestTest123", "Firstname", "Lastname")
+        expect(result1.status).toBe(400)
+        expect(result2.status).toBe(400)
+        expect(result3.status).toBe(200)
+        expect(typeof result3.user_id).toBe("number")
+        expect(result4.status).toBe(409)
     }
 })
