6b0c048ddb7b61e3250a5a20cc4008d49436512d
Wardur <haasdl2000@gmail.com>
2022-06-10 05:46:03 +0200

    working on testing

--- src/routes/accountFunctionality.test.ts
+++ src/routes/accountFunctionality.test.ts
@@ -4,6 +4,37 @@ process.env.DB_USER = process.env.DB_USER_TEST
 process.env.DB_PASSWORD = process.env.DB_PASSWORD_TEST
 process.env.DB_DATABASE = process.env.DB_DATABASE_TEST
 
+// @ts-ignore
+import mysql from 'mysql-await'
+
+//create mysql connection to export
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
 import * as func from "./accountFunctionality"
 
-test("No Test yet",()=>{})
+
+beforeEach(async () => {
+    if (process.env.TEST_DB) {
+        try {
+            await connection.awaitQuery("TRUNCATE TABLE invitation")
+            await connection.awaitQuery("TRUNCATE TABLE teammember")
+            await connection.awaitQuery("TRUNCATE TABLE team")
+            await connection.awaitQuery("TRUNCATE TABLE user")
+        } catch (e) {
+            console.error(e)
+        }
+    }
+})
+
+
+test("No Test yet", () => {
+    if (process.env.TEST_DB) {
+
+    }
+})
